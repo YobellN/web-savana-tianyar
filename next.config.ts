7e92dd3ef7c15482e574727d2e4ebef1434a10e7
YobellN <yobelnugroho17@gmail.com>
2025-07-13 11:30:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL('https://images.pexels.com/**')],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   }
 };
 
